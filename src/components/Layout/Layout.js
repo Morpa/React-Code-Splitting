eefdeb77bdb46b07766e3ed58b740b6aac40cc92
Morpa <andremorpa@gmail.com>
2021-12-06 21:19:33 +0000

    adicionando lazy, suspense, prefetch and preload as paginas

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,20 +1,15 @@
-import React from "react";
-import {
-  Route,
-  Switch,
-  Redirect,
-  withRouter,
-} from "react-router-dom";
+import React, { lazy, Suspense } from "react";
+import { Route, Switch, Redirect, withRouter } from "react-router-dom";
 import classnames from "classnames";
-import {Box, IconButton, Link} from '@material-ui/core'
-import Icon from '@mdi/react'
+import { Box, IconButton, Link } from "@material-ui/core";
+import Icon from "@mdi/react";
 
 //icons
 import {
   mdiFacebook as FacebookIcon,
   mdiTwitter as TwitterIcon,
   mdiGithub as GithubIcon,
-} from '@mdi/js'
+} from "@mdi/js";
 
 // styles
 import useStyles from "./styles";
@@ -23,17 +18,33 @@ import useStyles from "./styles";
 import Header from "../Header";
 import Sidebar from "../Sidebar";
 
-// pages
-import Dashboard from "../../pages/dashboard";
-import Typography from "../../pages/typography";
-import Notifications from "../../pages/notifications";
-import Maps from "../../pages/maps";
-import Tables from "../../pages/tables";
-import Icons from "../../pages/icons";
-import Charts from "../../pages/charts";
-
 // context
 import { useLayoutState } from "../../context/LayoutContext";
+
+// pages
+const Dashboard = lazy(() =>
+  import(
+    /* webpackChunkName: "Dashboard" */ /* webpackPreload: true */ "../../pages/dashboard"
+  ),
+);
+const Typography = lazy(() =>
+  import(/* webpackChunkName: "Typography" */ "../../pages/typography"),
+);
+const Notifications = lazy(() =>
+  import(/* webpackChunkName: "Notifications" */ "../../pages/notifications"),
+);
+const Maps = lazy(() =>
+  import(/* webpackChunkName: "Maps" */ "../../pages/maps"),
+);
+const Tables = lazy(() =>
+  import(/* webpackChunkName: "Tables" */ "../../pages/tables"),
+);
+const Icons = lazy(() =>
+  import(/* webpackChunkName: "Icons" */ "../../pages/icons"),
+);
+const Charts = lazy(() =>
+  import(/* webpackChunkName: "Charts" */ "../../pages/charts"),
+);
 
 function Layout(props) {
   var classes = useStyles();
@@ -43,16 +54,17 @@ function Layout(props) {
 
   return (
     <div className={classes.root}>
-        <>
-          <Header history={props.history} />
-          <Sidebar />
-          <div
-            className={classnames(classes.content, {
-              [classes.contentShift]: layoutState.isSidebarOpened,
-            })}
-          >
-            <div className={classes.fakeToolbar} />
-            <Switch>
+      <>
+        <Header history={props.history} />
+        <Sidebar />
+        <div
+          className={classnames(classes.content, {
+            [classes.contentShift]: layoutState.isSidebarOpened,
+          })}
+        >
+          <div className={classes.fakeToolbar} />
+          <Switch>
+            <Suspense fallback={<>Loading...</>}>
               <Route path="/app/dashboard" component={Dashboard} />
               <Route path="/app/typography" component={Typography} />
               <Route path="/app/tables" component={Tables} />
@@ -65,84 +77,64 @@ function Layout(props) {
               <Route path="/app/ui/maps" component={Maps} />
               <Route path="/app/ui/icons" component={Icons} />
               <Route path="/app/ui/charts" component={Charts} />
-            </Switch>
-            <Box
-              mt={5}
-              width={"100%"}
-              display={"flex"}
-              alignItems={"center"}
-              justifyContent="space-between"
-            >
-              <div>
-                <Link
-                  color={'primary'}
-                  href={'https://flatlogic.com/'}
-                  target={'_blank'}
-                  className={classes.link}
-                >
-                  Flatlogic
-                </Link>
-                <Link
-                  color={'primary'}
-                  href={'https://flatlogic.com/about'}
-                  target={'_blank'}
-                  className={classes.link}
-                >
-                  About Us
-                </Link>
-                <Link
-                  color={'primary'}
-                  href={'https://flatlogic.com/blog'}
-                  target={'_blank'}
-                  className={classes.link}
-                >
-                  Blog
-                </Link>
-              </div>
-              <div>
-                <Link
-                  href={'https://www.facebook.com/flatlogic'}
-                  target={'_blank'}
-                >
-                  <IconButton aria-label="facebook">
-                    <Icon
-                      path={FacebookIcon}
-                      size={1}
-                      color="#6E6E6E99"
-                    />
-                  </IconButton>
-                </Link>
-                <Link
-                  href={'https://twitter.com/flatlogic'}
-                  target={'_blank'}
-                >
-                  <IconButton aria-label="twitter">
-                    <Icon
-                      path={TwitterIcon}
-                      size={1}
-                      color="#6E6E6E99"
-                    />
-                  </IconButton>
-                </Link>
-                <Link
-                  href={'https://github.com/flatlogic'}
-                  target={'_blank'}
-                >
-                  <IconButton
-                    aria-label="github"
-                    style={{marginRight: -12}}
-                  >
-                    <Icon
-                      path={GithubIcon}
-                      size={1}
-                      color="#6E6E6E99"
-                    />
-                  </IconButton>
-                </Link>
-              </div>
-            </Box>
-          </div>
-        </>
+            </Suspense>
+          </Switch>
+          <Box
+            mt={5}
+            width={"100%"}
+            display={"flex"}
+            alignItems={"center"}
+            justifyContent="space-between"
+          >
+            <div>
+              <Link
+                color={"primary"}
+                href={"https://flatlogic.com/"}
+                target={"_blank"}
+                className={classes.link}
+              >
+                Flatlogic
+              </Link>
+              <Link
+                color={"primary"}
+                href={"https://flatlogic.com/about"}
+                target={"_blank"}
+                className={classes.link}
+              >
+                About Us
+              </Link>
+              <Link
+                color={"primary"}
+                href={"https://flatlogic.com/blog"}
+                target={"_blank"}
+                className={classes.link}
+              >
+                Blog
+              </Link>
+            </div>
+            <div>
+              <Link
+                href={"https://www.facebook.com/flatlogic"}
+                target={"_blank"}
+              >
+                <IconButton aria-label="facebook">
+                  <Icon path={FacebookIcon} size={1} color="#6E6E6E99" />
+                </IconButton>
+              </Link>
+              <Link href={"https://twitter.com/flatlogic"} target={"_blank"}>
+                <IconButton aria-label="twitter">
+                  <Icon path={TwitterIcon} size={1} color="#6E6E6E99" />
+                </IconButton>
+              </Link>
+              <Link href={"https://github.com/flatlogic"} target={"_blank"}>
+                <IconButton aria-label="github" style={{ marginRight: -12 }}>
+                  <Icon path={GithubIcon} size={1} color="#6E6E6E99" />
+                </IconButton>
+              </Link>
+            </div>
+          </Box>
+        </div>
+      </>
     </div>
   );
 }
